b5fdc71bd36ec459cf13ca30449a48a3bd1deaaf
sheilambadi <mbadiatieno@gmail.com>
2018-09-06 09:47:52 +0300

    Add movie model content

--- src/main/java/com/sheilambadi/movieapi/model/MovieModel.java
+++ src/main/java/com/sheilambadi/movieapi/model/MovieModel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sheilambadi.movieapi.model;
 
 /**
@@ -10,5 +5,38 @@ package com.sheilambadi.movieapi.model;
  * @author sheilambadi
  */
 public class MovieModel {
+    private int movieId;
+    private String movieName;
+    private String movieSynopsis;
+
+    public MovieModel(int movieId, String movieName, String movieSynopsis) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.movieSynopsis = movieSynopsis;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieSynopsis() {
+        return movieSynopsis;
+    }
+
+    public void setMovieSynopsis(String movieSynopsis) {
+        this.movieSynopsis = movieSynopsis;
+    }
     
 }
