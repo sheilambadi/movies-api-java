6fb908d888a50a7549cbf1a2e63cf6883ba15928
sheilambadi <mbadiatieno@gmail.com>
2018-09-06 09:49:41 +0300

    Add jersey configuration

---
+++ src/main/java/com/sheilambadi/movieapi/utils/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.sheilambadi.movieapi.utils;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author sheilambadi
+ */
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application{
+    
+}
